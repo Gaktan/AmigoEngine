2e396bf8c5901230de1beb9dce0d18f61f0d6d7e
Gaktan <igaktan@gmail.com>
2019-04-07 19:45:21 +0200

    * Updated GenerateProjects.bat to copy latest executables

--- GenerateProjects.bat
+++ GenerateProjects.bat
@@ -1,1 +1,5 @@
+cd external\Sharpmake\
+py deploy_binaries.py --config Release --target-dir=..\..\tools\Sharpmake\
+cd ..\..\
+rem py tools\Sharpmake\CopySharpmakeExecutables.bat
 tools\Sharpmake\Sharpmake.Application.exe /sources("projects/sharpmake/main.sharpmake.cs")
