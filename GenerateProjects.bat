f4c50b8cc466b6e315d068f7dedb2bd8f807ff51
Gaktan <igaktan@gmail.com>
2019-06-09 00:10:37 +0200

    * Fixed GenerateProject.bat to automatically build Sharpmake

--- GenerateProjects.bat
+++ GenerateProjects.bat
@@ -1,5 +1,7 @@
 @echo off
 cd external\Sharpmake\
+bootstrap-sharpmake.bat
+CompileSharpmake.bat Sharpmake.Application/Sharpmake.Application.csproj Release AnyCPU
 py deploy_binaries.py --config Release --target-dir=..\..\tools\Sharpmake\
 cd ..\..\
 
