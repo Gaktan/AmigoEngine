344c5b8f0eeecb1ebcc72cea780b5aa9ec42acf7
Gaktan <igaktan@gmail.com>
2019-06-09 13:23:25 +0200

    * Fixed a bug where GenerateProject did nothing
    This was caused because bootstrap-sharpmake.bat calls an exit and this closes the parent batch file. So we need to use call bootstrap-sharpmake.bat instead
    
    Also moved Sharpmake compilation and deployment to UpdateGitDependencies just because we don't need to compile it every time anymore

--- GenerateProjects.bat
+++ GenerateProjects.bat
@@ -1,9 +1,4 @@
 @echo off
-cd external\Sharpmake\
-bootstrap-sharpmake.bat
-CompileSharpmake.bat Sharpmake.Application/Sharpmake.Application.csproj Release AnyCPU
-py deploy_binaries.py --config Release --target-dir=..\..\tools\Sharpmake\
-cd ..\..\
 
 rem py tools\Sharpmake\CopySharpmakeExecutables.bat
 tools\Sharpmake\Sharpmake.Application.exe /sources("projects/sharpmake/main.sharpmake.cs")
--- UpdateGitDependencies.bat
+++ UpdateGitDependencies.bat
@@ -3,3 +3,10 @@
 git submodule init
 git submodule update
 git submodule update --recursive --remote
+
+rem Compile SharpMake and deploy it
+cd external\Sharpmake\
+call bootstrap-sharpmake.bat
+CompileSharpmake.bat Sharpmake.Application/Sharpmake.Application.csproj Release AnyCPU
+py deploy_binaries.py --config Release --target-dir=..\..\tools\Sharpmake\
+cd ..\..\
