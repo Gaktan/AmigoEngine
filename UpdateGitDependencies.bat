e1789b169290b4aa98b8df3eae7257c16693252a
Gaktan <igaktan@gmail.com>
2019-07-07 13:12:02 +0200

    Update UpdateGitDependencies.bat

--- UpdateGitDependencies.bat
+++ UpdateGitDependencies.bat
@@ -7,6 +7,6 @@ git submodule update --recursive --remote
 rem Compile SharpMake and deploy it
 cd external\Sharpmake\
 call bootstrap-sharpmake.bat
-CompileSharpmake.bat Sharpmake.Application/Sharpmake.Application.csproj Release AnyCPU
+call CompileSharpmake.bat Sharpmake.Application/Sharpmake.Application.csproj Release AnyCPU
 py deploy_binaries.py --config Release --target-dir=..\..\tools\Sharpmake\
 cd ..\..\
